7b91549f6fc67060644a8a9d2be4f5af003049a8
Mathias <mnstokholm@gmail.com>
2022-05-06 10:52:40 +0000

    Data loaded AGAIN

--- R/01_load.R
+++ R/01_load.R
@@ -1,13 +1,13 @@
 library("readxl") # To read excel files
 
 envDat <- read_excel("_raw/Raw_Env_Data.xlsx")
-write.csv(envDat, "data/01_Env_Data_load.csv", 
+write.csv(envDat, "data/01_env_load.csv", 
           row.names = FALSE)
 
 OTU <- read_excel("_raw/Raw_OTU_Counts.xls")
-write.csv(OTU, "data/01_OTU_Counts_load.csv",
+write.csv(OTU, "data/01_otu_counts_load.csv",
           row.names = TRUE)
 
 OTU_meta <- read_excel("_raw/Raw_OTU_Meta_Data.xlsx")
-write.csv(OTU_meta, "data/01_OTU_Meta_Data_load.csv",
+write.csv(OTU_meta, "data/01_otu_meta_load.csv",
           row.names = TRUE)
