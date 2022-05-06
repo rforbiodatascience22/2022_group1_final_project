adfd229f518b8179b3894857fd15760010983e35
Mathias <mnstokholm@gmail.com>
2022-05-06 17:18:34 +0000

    Wrangling done

---
+++ R/00_doit.R
@@ -0,0 +1,10 @@
+# Load data
+source("R/01_load.R")
+# Clean it up
+source("R/02_clean.R")
+# Prepare data for analysis
+source("R/03_augment.R")
+
+# Do analysis
+
+
--- R/02_clean.R
+++ R/02_clean.R
@@ -4,15 +4,20 @@ library(tidyverse)
 envDat <- read_csv("data/01_env_load.csv", col_types = "c") %>% 
   rename(dates = "Date (dd.mm.yy)") 
 
-OTU <- read_csv("data/01_otu_counts_load.csv")
+OTU <- read_csv("data/01_otu_counts_load.csv") %>% 
+  select(-Rownames)
 
-OTU_meta <- read_csv("data/01_otu_meta_load.csv")
+OTU_meta <- read_csv("data/01_otu_meta_load.csv") %>% 
+  rename(OTU_id = "OTU id") %>% 
+  mutate(OTU_id = OTU_id %>% str_replace(" ", "_"))
 
 
 # write files
-write.csv(envDat, "data/02_env_load.csv", 
+write.csv(envDat, "data/02_env_clean.csv", 
           row.names = FALSE)
-write.csv(OTU, "data/02_otu_counts_load.csv",
-          row.names = TRUE)
-write.csv(OTU_meta, "data/02_otu_meta_load.csv",
-          row.names = TRUE)
+write.csv(OTU, "data/02_otu_counts_clean.csv",
+          row.names = FALSE)
+write.csv(OTU_meta, "data/02_otu_meta_clean.csv",
+          row.names = FALSE)
+
+rm(list = c("envDat", "OTU", "OTU_meta"))
