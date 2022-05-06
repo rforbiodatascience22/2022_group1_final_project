b2f4376f94446179bca8d5544df447840b68275c
Mathias <mnstokholm@gmail.com>
2022-05-06 10:44:26 +0000

    Data Loaded

--- R/01_load.R
+++ R/01_load.R
@@ -1,8 +1,17 @@
 library("readxl") # To read excel files
 library("tidyverse") # It's just neat
 
+envDat <- read_excel("_raw/Raw_Env_Data.xlsx")
+write.csv(envDat, "data/Env_Data.csv", 
+          sep = ",", 
+          row.names = FALSE)
 
-otu <- read_excel("data/MEC-14-1145-OTUtab_piconanopool_subsamp5553_correctversion.xls")
-env <- read_excel("data/Env_data_mec-14-1145.xlsx")
-tab2 <- read_excel("data/Table_S2_Egge_et_al_2015_jeukmic.xlsx")
-tab4 <- read.csv("data/figure4_data.csv", sep =";")
+OTU <- read_excel("_raw/Raw_OTU_Counts.xls")
+write.csv(OTU, "data/OTU_Counts.csv",
+          sep = ",",
+          row.names = TRUE)
+
+OTU_meta <- read_excel("_raw/Raw_OTU_Meta_Data.xlsx")
+write.csv(OTU_meta, "data/OTU_Meta_Data.csv",
+          sep = ",",
+          row.names = TRUE)
